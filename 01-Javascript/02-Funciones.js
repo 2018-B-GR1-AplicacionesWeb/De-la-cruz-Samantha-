1bfc8b2edce78913218a5be95636ecf62f0571b2
sammy de la cruz <samy_samantha_5@hotmail.com>
2018-10-15 10:59:16 -0500

    Update 01-Javascript/02-Funciones.js

--- 01-Javascript/02-Funciones.js
+++ 01-Javascript/02-Funciones.js
@@ -68,3 +68,24 @@ function sumarNumerosDesdeUnArreglo(numeros) {
 
     console.log(saludarenUperCase("samy"));
 }
+function saludarEnUpperCase(nombre, funcion) {
+    return `Hola ${funcion(nombre)}`; // template strings
+}
+console.log(saludarEnUpperCase("adrian", convertirStringEnMayuscula));
+console.log(saludarEnUpperCase("VICENTE", convertirStringEnMinuscula));
+console.log(saludarEnUpperCase("Buen dia", anadirPuntoAlFinal));
+function convertirStringEnMayuscula(texto) {
+    return texto.toUpperCase();
+}
+function convertirStringEnMinuscula(texto) {
+    return texto.toLowerCase();
+}
+function anadirPuntoAlFinal(texto) {
+    return texto + ".";
+}
+function primeraLetraEnMayuscula(texto) {
+    var primeraLetraMayuscula = texto[0].toUpperCase();
+    var restoPalabra = texto.slice(1, texto.length);
+    return primeraLetraMayuscula + restoPalabra;
+}
+console.log(saludarEnUpperCase("adrian",primeraLetraEnMayuscula));
