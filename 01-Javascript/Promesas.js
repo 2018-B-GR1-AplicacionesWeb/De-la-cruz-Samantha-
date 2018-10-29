24ad6f635af453df8f12a2735727c8422adc538c
sammy de la cruz <samy_samantha_5@hotmail.com>
2018-10-29 11:06:25 -0500

    Promesa

--- 01-Javascript/Promesas.js
+++ 01-Javascript/Promesas.js
@@ -45,21 +45,48 @@ nuevaPromesaLectura
         }
     );
 
+const nuevaPromesaAppendFile = (nombreArchivo,contenidoArchivo) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(nombreArchivo,'utf-8',
+                (error,contenidoArchivo)=>{
+                    if (error){
+                        fs.writeFile(nombreArchivo,contenidoArchivo,
+                            (err)=>{
+                                if (err) {
+                                    reject(console.error('Error escribiendo'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
 
-const appendFilePromesa = new Promise(
-    (resolve, reject) =>{
-        fs.readFile('06-texto.txt', 'utf-8',
-            (err, contenidoArchivo) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(contenidoArchivo);
+                    }else{
+                        fs.writeFile(nombreArchivo,contenidoArchivo+contenidoArchivo,
+                            (err)=>{
+                                if (err) {
+                                    reject(console.error('Error escribiendo'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
+                    }
+
                 }
-            })
 
+
+            );
+
+        }
+    );
+};
+
+nuevaPromesaAppendFile()
     .then(
         (resultadoOk) => {
             console.log('Todo bien', resultadoOk);
+            return nuevaPromesaAppendFile(contenidoArchivo,contenidoArchivo)
 
         }
     )
@@ -67,5 +94,14 @@ const appendFilePromesa = new Promise(
         (resultadoError) => {
             console.log('Algo malo paso', resultadoError);
         }
+    );
+
+nuevaPromesaAppendFile('06-texto3.text','\n ',
+    (contenidoArchivo,error)=>{
+        if(error){
+            console.log('Error',error)
+        }else{
+            console.log(contenidoArchivo)
+        }
     }
-      );
+);
